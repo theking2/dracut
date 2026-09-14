e996d9dc128630b7c54c5e0e17007466412002df
Johannes Kingma <theking2@king.ma>
2026-09-14 17:36:43 +0200

    Improve warning message for Bluetooth module inclusion

--- modules.d/70bluetooth/module-setup.sh
+++ modules.d/70bluetooth/module-setup.sh
@@ -15,7 +15,7 @@ check() {
         # and if Appearance is set to the value defined for keyboard (0x03C1)
         [ -d "/sys/class/bluetooth" ] && grep -qsiE -e 'Class=0x[0-9a-f]{3}5[4c]0' -e 'Appearance=0x03c1' /var/lib/bluetooth/*/*/info \
             && [[ " $dracutmodules $add_dracutmodules $force_add_dracutmodules $prefer_dracutmodules " != *\ bluetooth\ * ]] \
-            && dwarn "If you need to use bluetooth, please include it explicitly."
+            && dwarn "If you need to use Bluetooth during boot, please add the "bluetooth" Dracut module explicitly."
     fi
 
     return 255
